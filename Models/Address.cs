69b0b3f3e50c1a45b96cf9ad67141ce4ff7c42e5
Felix Veras <fcarvajal44@gmail.com>
2023-02-19 17:05:35 -0400

    Creating project

---
+++ Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CustomerDirectionsWebApp.Models
+{
+    public class Address
+    {
+        [Key]
+        public int AddressID { get; set; }
+
+        [Required]
+        [Display(Name = "Direccion")]
+        [StringLength(180, ErrorMessage = "Este campo admite {1} caracteres como maximo.")]
+        public string AddressName { get; set; }
+
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerDirectionsWebApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        [StringLength(20, ErrorMessage = "Este campo admite {1} caracteres como maximo.")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [Display(Name = "Apellido")]
+        [StringLength(20, ErrorMessage = "Este campo admite {1} caracteres como maximo.")]
+        public string LastName { get; set; }
+        
+        [Display(Name = "Nombre Completo")]
+        public string FullName { get { return this.FirstName + " " + this.LastName; } }
+
+        [Required]
+        [Display(Name = "Correo Electronico")]
+        [StringLength(100, ErrorMessage = "Este campo admite {1} caracteres como maximo.")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Telefono")]
+        [StringLength(12, ErrorMessage = "Este campo admite {1} caracteres como maximo.")]
+        public string Phone { get; set; }
+
+        public virtual ICollection<Address> Addresses { get; set; }
+    }
+}
